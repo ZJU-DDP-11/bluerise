860e29b1f01cfd901c28d16fa5110fcb9332e8d7
LouisYe <yeluyuptd@163.com>
2013-05-21 15:45:11 +0800

    给后台页面文件名加下划线_

--- device.php
+++ device.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include("func_info.php");
+include("_func_info.php");
 /*
 if ($_SESSION["authen"]==false) {
 	gotoThePage($homepage);
@@ -12,36 +12,7 @@ if ($_SESSION["authen"]==false) {
 <?php include 'header.php'; ?>
 
 <a href="#addDevice" role="button" class="btn span2 offset9 btn-success" data-toggle="modal">Create Device</a>
-
-<?php
-#$userid = $_SESSION['userid'];
-$userid = 1;
-$result = mysqli_query($dbcon, "select * from $tb_device where userid='$userid';");
-?>
-
-<div class = 'row span14'>
-	<table class = "">
-	<br>
-		<td>Deivce Name</td>
-		<td>Description</td>
-		<td>Manage</td>
-	</br>
-<?php
-while ( ($device = mysql_fetch_array($result)) ) {
-	echo "<tr>";
-	echo "<td>".$device['deviceName']."</td>";
-	echo "<td>".$device['description']."</td>";
-	echo "<td>";
-	echo "<a class='btn btn-primary btn-large btn-block' href='#'>Edit</a>";
-	echo "<a class='btn btn-primary btn-large btn-block' href='#'>Delete</a>";
-	echo "</td>";
-	echo "</tr>";
-}
-?>
-	</table>
-</div>
-
-	<div id="addDevice" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div id="addDevice" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 	  <div class="modal-header">
 	    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">X</button>
 	    <h3 id="myModalLabel">Configure your Device</h3>
@@ -56,7 +27,39 @@ while ( ($device = mysql_fetch_array($result)) ) {
 	    <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
 	    <button class="btn btn-primary">Save Configurations</button>
 	  </div>
-	</div>
+</div>
+
+<?php
+#$userid = $_SESSION['userid'];
+$userid = 1;
+$result = mysqli_query($dbcon, "select * from $tb_device where userid='$userid';") or die("Fail selection!");
+?>
+
+<div name="device table" class = 'span15'>
+	<table class = 'span14' style='text-align: center;'>
+	<tr>
+		<td class='span4'><h2>Deivce ID</h2></td>
+		<td class='span4'><h2>Device Name</h2></td>
+		<td class='span5'><h2>Manage</h2></td>
+	</tr>
+
+<?php
+while ( ($device = mysqli_fetch_array($result)) ) {
+	echo "<tr>";
+	echo "<td class='span4'><h4>".$device['id']."</h4></td>";
+	echo "<td class='span4'><h4>".$device['deviceName']."</h4></td>";
+	echo "<td class='span5'>";
+	echo "<a class='btn btn-medium btn-primary span1' href='#'>Edit</a>";
+	echo "<a class='btn btn-medium btn-danger span1' href='#'>Delete</a>";
+	echo "<a class='btn btn-medium btn-info span1' href='#'>Info</a>";
+	echo "</td>";
+	echo "</tr>";
+}
+?>
+	</table>
+</div>
+
+	
 
 
 
