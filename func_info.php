3cf8924a030a91b60f4bb31baf388f7315acca0d
LouisYe <yeluyuptd@163.com>
2013-05-21 14:49:30 +0800

    func_info

--- func_info.php
+++ func_info.php
@@ -1,11 +1,14 @@
 <?php
 $homepage	= "index.php";
-$dbhost 	= "10.71.10.71";
+$dbhost 	= "localhost";
 $dbaccount	= "ddp";
 $dbpassword	= "ddprocks";
 $dbname		= "ddp";
+$tb_device	= "device";
+$tb_user	= "user";
+$tb_data	= "data";
 
-$dbcon=mysqli_connect($dbhost, $dbaccount, $dbpassword, $dbname) or die("Fail to connect to database~");
+$dbcon=mysqli_connect($dbhost, $dbaccount, $dbpassword, $dbname) or die("Fail to connect to database: ".mysqli_connect_error());
 
 function gotoThePage($page) { //Not appropriate for the pure PHP page without HTML outputs;
 		echo "<script language='javascript' type='text/javascript'> window.location.href='".$page."'</script>";
