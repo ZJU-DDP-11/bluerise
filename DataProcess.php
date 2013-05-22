f2598c4fb509892ebf3acd1f983d1d952253b7f5
francushu <huhexiang88@gmail.com>
2013-05-22 19:09:44 +0800

    给静态地图添加了一个marker
    map.html用于做修改坐标的实验用，是一个临时文件

--- DataProcess.php
+++ DataProcess.php
@@ -68,7 +68,7 @@
 		  	  font-size: 15px;
 		  	  color: inherit;
 		  	  font-weight: bold;
-		  	  height:150px;
+		  	  height:120px;
 		  	  width:90%;
 		  	  margin:0 auto;
 		  	  
@@ -78,17 +78,21 @@
 	$script = "
 	<script src='https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false'></script>
 	<script>
+		var deviceCenter = new google.maps.LatLng(39.92,116.46)
 		var map;
 		function initialize() {
-		  var mapOptions = {
-			zoom: 8,
-			center: new google.maps.LatLng(39.92,116.46),
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
-		  map = new google.maps.Map(document.getElementById('map-canvas'),
-			  mapOptions);
+			var mapOptions = {
+				zoom: 8,
+				center: deviceCenter,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+		 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+		 	var marker = new google.maps.Marker({
+		 		position:deviceCenter,
+		 	});
+		  	marker.setMap(map);
 		}
-		
+
 		google.maps.event.addDomListener(window, 'load', initialize);
 	</script>";
 	include("header.php");
@@ -123,6 +127,9 @@
 			</div>
 			<div class="Area">
 				Description
+			</div>
+			<div class="namecard-row">
+				<button class="btn btn-primary" onclick="location.href='#'">Edit</button>
 			</div>
 		</div>
 		<div id="data-bar">
