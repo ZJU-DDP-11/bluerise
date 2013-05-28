b7c3d92e3a0e1b4b77890604b36cb58832e4f164
francushu <huhexiang88@gmail.com>
2013-05-28 11:13:31 +0800

    footer与header的改变

--- footer.php
+++ footer.php
@@ -1,22 +1,22 @@
-		<footer class="offset1"style="width:1048px;white-space:nowrap;">
-			<div class="container"style="width:1048px;white-space:nowrap;">
-				<div class="row">
-					<table class="span12">
-						<div class="span2"><a href="#">site map</a></div>
-						<div class="span2"><a href="#">Join us</a></div>
-						<div class="span2"><a href="#">Privacy policy</a></div>
-						<div class="span2"><a href="#">Team of use</a></div>
-						<div class="span2"><a href="#">contact</a></div>
-						<div class="span12">
-						<h4>Where the Internet of things is being built</h4>
-						<p>Connect devices and apps on the BlueRise platform,
-							exchange data and ideas with developers, and bring smart
-							products to the world!</p>
-						</div>
-					</table>
+	</div>
+	<footer id="footer">
+		<div class="container" style="white-space:nowrap;">
+			<div class="span12">
+				<div class="span12" style="text-align:center;margin:10px 0 10px 0">
+					<div class="span2" style="margin:0px 15px 0px 30px;"><a href="#">Site map</a></div>
+					<div class="span2" style="margin:0px 15px 0px 30px;"><a href="#">Join us</a></div>
+					<div class="span2" style="margin:0px 15px 0px 30px;"><a href="#">Privacy policy</a></div>
+					<div class="span2" style="margin:0px 15px 0px 30px;"><a href="#">Terms of use</a></div>
+					<div class="span2" style="margin:0px 15px 0px 30px;"><a href="#">Contact</a></div>
+				</div>
+				<div class="span12" style="text-align:left;">
+				<h4>Where the Internet of things is being built</h4>
+				<p>Connect devices and apps on the BlueRise platform,
+					exchange data and ideas with developers, and bring smart
+					products to the world!</p>
 				</div>
 			</div>
-		</footer>
-	</div>
+		</div>
+	</footer>
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html> 
 <html lang="en">
 <head>
 	  <meta charset="utf-8">
@@ -8,6 +9,17 @@
 		  .demo-panel-title{
 			  margin-left:20px;
 		  }
+		  header{
+			  margin-left: auto;
+			  margin-right:auto;
+			  width: 1000px;
+		  }
+		  footer{
+			  margin-top: 500px;
+		  	  margin-left: auto;
+		  	  margin-right:auto;
+		  	  width: 1000px;
+		    }
 		  #container{
 			  margin-left: auto;
 			  margin-right:auto;
@@ -44,61 +56,63 @@
 </head>
 
 <body>
-<div id="container">
-	<h1 class="demo-panel-title">BlueRise</h1>
-	<div class="accordion" id="accordion2">
-	   	<div class="accordion-group" >
-	   		<div class="accordion-heading row">
-	   			<div class="todo-icon fui-man-16" style="padding:0;margin:5px 5px 0 40px;"></div>
-	   			<div id="user-account" class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
-	   				example@example.com 
-	   			</div>
-	   		</div>
-	   		<div id="collapseOne" class="accordion-body collapse">
-	   			<div class="todo-icon fui-settings-16" style="padding:0px;margin:5px 5px 0 20px;"></div>
-	   			<div class="accordion-inner">
-	   				User setting
-	   			</div>
-	   			<div class="todo-icon fui-video-16" style="padding:0px;margin:5px 5px 0 20px;"></div>
-	   			<div class="accordion-inner">
-	   				Device
-	   			</div>
-	   			<div class="todo-icon fui-cross-16" style="padding:0px;margin:5px 5px 0 20px;"></div>
-	   			<div class="accordion-inner">
-	   				Log-out
-	   			</div>
-	   		</div>
-	   	</div>
-	</div>
-	<div class="span12" style="width:960px;">
-		<div class="navbar navbar-inverse">
-			<div class="navbar-inner">
-				<div class="container">
-					<div class="nav-collapse collapse">
-						<ul class="nav">
-							<li>
-								<a href="/">
-									Home
-								</a>
-							</li>
-							<li >
-								<a href="/support.php">
-									Support
-								</a>
-							</li>
-							<li>
-								<a href="/how.php">
-									How it Works
-								</a>
-							</li>
-							<li>
-								<a href="/about.php">
-									About Us
-								</a>
-							</li>
-						</ul>
-					</div><!--/.nav-collapse -->
-				</div><!--container-->
-			</div><!--navbar-inner-->
+	<header class="header">
+		<h1 class="demo-panel-title">BlueRise</h1>
+		<div class="accordion" id="accordion2">
+			<div class="accordion-group" >
+			   	<div class="accordion-heading row">
+			   		<div class="todo-icon fui-man-16" style="padding:0;margin:5px 5px 0 40px;"></div>
+			   		<div id="user-account" class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
+			   			example@example.com 
+			   		</div>
+			   	</div>
+			   	<div id="collapseOne" class="accordion-body collapse">
+			   		<div class="todo-icon fui-settings-16" style="padding:0px;margin:5px 5px 0 20px;"></div>
+			   		<div class="accordion-inner">
+			   			User setting
+			   		</div>
+			   		<div class="todo-icon fui-video-16" style="padding:0px;margin:5px 5px 0 20px;"></div>
+			   		<div class="accordion-inner">
+			   			Device
+			   		</div>
+			   		<div class="todo-icon fui-cross-16" style="padding:0px;margin:5px 5px 0 20px;"></div>
+			   		<div class="accordion-inner">
+			   			Log-out
+			   		</div>
+			   	</div>
+			</div>
 		</div>
-	</div>
+		<div class="span12" style="width:960px;">
+			<div class="navbar navbar-inverse">
+				<div class="navbar-inner">
+					<div class="container">
+						<div class="nav-collapse collapse">
+							<ul class="nav">
+								<li>
+									<a href="/">
+										Home
+									</a>
+								</li>
+								<li >
+									<a href="/support.php">
+										Support
+									</a>
+								</li>
+								<li>
+									<a href="/how.php">
+										How it Works
+									</a>
+								</li>
+								<li>
+									<a href="/about.php">
+										About Us
+									</a>
+								</li>
+							</ul>
+						</div><!--/.nav-collapse -->
+					</div><!--container-->
+				</div><!--navbar-inner-->
+			</div>
+		</div>		
+	</header>
+	<div id="container">
