beb20c718acd5f51f2ef133c61d1540a874904bf
francushu <huhexiang88@gmail.com>
2013-05-28 18:50:30 +0800

    eidt the initial map allocation

--- edit_device.php
+++ edit_device.php
@@ -7,6 +7,13 @@ if ($_SESSION["authen"]==false) {
 	exit;
 }
  */
+ $deviceid = $_GET['id'];
+ $userid = 1;//$_SESSION['userid'];
+ $cmd = "select * from $tb_device where id='$deviceid' and userid = '$userid';";
+ //echo $cmd;
+ $result = mysqli_query($dbcon, $cmd) or die("database Fail selection!");
+ $row = mysqli_fetch_array($result);
+ 
 $css = "
 		<style>
 		  #map-bar{
@@ -21,25 +28,84 @@ $css = "
 		    }
 		</style>";
 
-$script = <<<SCRIPT
+if($row['longitude'] == NULL || $row['latitude'] == NULL )
+{
+	$script ="
+		<script
+		src='http://maps.googleapis.com/maps/api/js?key=AIzaSyDY0kkJiTPVd2U7aTOAwhc9ySH6oHxOIYM&sensor=false'>
+		</script>
+		<script>
+		var map;
+		var myCenter=new google.maps.LatLng(39.92,116.46);
+		var marker;
+		var infowin;
+	
+		function initialize()
+		{
+			var mapConfig = {
+				center:myCenter,
+				zoom:5,
+				mapTypeId:google.maps.MapTypeId.ROADMAP
+		};
+		map = new google.maps.Map(document.getElementById('googleMap'),mapConfig);
+	
+		google.maps.event.addListener(map, 'click', function(event) {
+			placeMarker(event.latLng);
+			});
+		}
+	
+		function placeMarker(location) {
+			if(marker){
+			 	marker.setMap(null);
+			}
+	
+			marker = new google.maps.Marker({
+				position: location,
+				map: map,
+				title: 'My Device Location'
+			});
+			infowin = new google.maps.InfoWindow({
+				content: 'Your Device\'s Location:<br>'+'Latitude: ' + location.lat() + '<br>Longitude: ' + location.lng()
+		  	});
+		  	infowin.open(map,marker);
+	
+		  	google.maps.event.addListener(marker, 'click', function() {
+				infowin.open(map, this);
+			});
+	
+	             document.getElementById('lng').value = location.lng();
+	             document.getElementById('lat').value = location.lat();
+		}
+	
+		google.maps.event.addDomListener(window, 'load', initialize);
+	</script>";
+}else
+{
+	$script = "
 	<script
-	src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDY0kkJiTPVd2U7aTOAwhc9ySH6oHxOIYM&sensor=false">
+	src='http://maps.googleapis.com/maps/api/js?key=AIzaSyDY0kkJiTPVd2U7aTOAwhc9ySH6oHxOIYM&sensor=false'>
 	</script>
 	<script>
 	var map;
-	var myCenter=new google.maps.LatLng(51.508742,-0.120850);
 	var marker;
 	var infowin;
+	var myCenter = new google.maps.LatLng(".$row['latitude'].", ".$row['longitude'].");
 
 	function initialize()
 	{
 		var mapConfig = {
 			center:myCenter,
-			zoom:5,
+			zoom:8,
 			mapTypeId:google.maps.MapTypeId.ROADMAP
 	};
-	map = new google.maps.Map(document.getElementById("googleMap"),mapConfig);
-
+	map = new google.maps.Map(document.getElementById('googleMap'),mapConfig);
+	
+	marker = new google.maps.Marker({
+		map: map,
+		position: myCenter,
+		title:  'My Device Location'
+	});
+	
 	google.maps.event.addListener(map, 'click', function(event) {
 		placeMarker(event.latLng);
 		});
@@ -64,31 +130,26 @@ $script = <<<SCRIPT
 			infowin.open(map, this);
 		});
 
-             document.getElementById("lng").value = location.lng();
-             document.getElementById("lat").value = location.lat();
+			 document.getElementById('lng').value = location.lng();
+			 document.getElementById('lat').value = location.lat();
 	}
 
 	google.maps.event.addDomListener(window, 'load', initialize);
-</script>
-SCRIPT;
+</script>";
+}
+
+
  include 'header.php'; ?>
 
 
 <form method="post" action="_manage_device.php">
 	<div class="container">
-		<?php
-		$deviceid = $_GET['id'];
-		$userid = 1;//$_SESSION['userid'];
-		$cmd = "select * from $tb_device where id='$deviceid' and userid = '$userid';";
-		//echo $cmd;
-		$result = mysqli_query($dbcon, $cmd) or die("database Fail selection!");			
+		<?php		
 
 		if (mysqli_num_rows($result) == 0) {
 			echo "<h1>No Such Device!</h1>";
 			exit;
 		}
-
-		$row = mysqli_fetch_array($result);
 		?> 
 		
 		<h3 class="demo-panel-title offset1" align="center">Device Info</h3>
@@ -102,7 +163,7 @@ SCRIPT;
 				<input name="deviceName" type="text" value="<?php echo $row['deviceName']; ?>" placeholder="device name" class="span3" />
 			</div>
 			<div class="span8 offset2">
-				<textarea name="description" value="<?php echo $row['description']; ?>" placeholder="Description" rows="5" class="span8"></textarea>
+				<textarea name="description" placeholder="Description" rows="5" class="span8"><?php echo $row['description']; ?></textarea>
 			</div>
 		</div>
 		<input id='lng' name="longitude" type="hidden" value=''>
