a129390907fe4c207ed7ed6a36cbbc474385cba6
Michael Gao <gaofanmichael@163.com>
2013-05-28 13:27:14 +0800

    again

--- jiexi.php
+++ jiexi.php
@@ -11,7 +11,7 @@
 //$dbcon=mysqli_connect($dbhost, $dbaccount, $dbpassword, $dbname) or die("Fail to connect to database: ".mysqli_connect_error());
 	//$dbcon=mysql_connect("localhost","root") or die("数据库服务器连接错误".mysql_error());
 	//mysql_select_db("ddp",$dbcon) or die("数据库访问错误".mysql_error());
-	$a=file_get_contents('php://input')
+	$a=file_get_contents('php://input');
 
     $sql=mysqli_query( $dbcon,"insert into testjson (json) values('$a');") or die("jkjkjk");
 
