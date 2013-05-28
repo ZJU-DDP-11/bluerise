c87c343fb99cd56b407871bb36b6d02846c2c688
Michael Gao <gaofanmichael@163.com>
2013-05-28 12:28:35 +0800

    d

--- jiexi.php
+++ jiexi.php
@@ -3,6 +3,6 @@
 	include("_func_info.php");
    //$jsonReceiveData = json_decode($_POST);
    print_r($_POST);
-   $sql=mysql_query("insert into testjson(json)values($_POST)");
+   $sql=mysqli_query($dbcon, "insert into testjson(json)values($_POST)");
 
 ?>
