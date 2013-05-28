7c52796afad7537736121081294a31d6912ffc64
LouisYe <yeluyuptd@163.com>
2013-05-28 13:03:38 +0800

    try1

--- _func_info.php
+++ _func_info.php
@@ -3,14 +3,16 @@ $homepage	= "index.php";
 $dbhost 	= "localhost";
 $dbaccount	= "ddp";
 $dbpassword	= "ddprocks";
-$dbname	= "ddp";
+$dbname		= "ddp";
 $tb_device	= "device";
 $tb_user	= "user";
 $tb_data	= "data";
+$devicepage	= "device.php";
 
 $dbcon=mysqli_connect($dbhost, $dbaccount, $dbpassword, $dbname) or die("Fail to connect to database: ".mysqli_connect_error());
 
 function gotoThePage($page) { //Not appropriate for the pure PHP page without HTML outputs;
 		echo "<script language='javascript' type='text/javascript'> window.location.href='".$page."'</script>";
 	}
+
 ?>
