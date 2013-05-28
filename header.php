bad7ceff343b189712065d27d0f31ebce3eafbd4
csvenja <c.svenjax@gmail.com>
2013-05-28 12:43:11 +0800

    modified header

--- header.php
+++ header.php
@@ -1,25 +1,16 @@
-<!DOCTYPE html> 
 <html lang="en">
 <head>
 	  <meta charset="utf-8">
 	  <!-- Le styles -->
+	  <title>Blue Rise</title>
+	  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
 	  <link href="lib/css/bootstrap.css" rel="stylesheet">
 	  <link href="lib/css/flat-ui.css" rel="stylesheet">
 	  <link href="lib/css/bootstrap-responsive.css" rel="stylesheet">
+	  <link rel="shortcut icon" href="lib/images/favicon.ico">
 	  <style>
 		  .demo-panel-title{
 			  margin-left:20px;
-		  }
-		  #footer{
-			  clear: both;
-			  width:960px;
-			  margin-left: 30px;
-			  position: relative;
-			  z-index: 10;
-			  height: 3em;
-			  margin-top: -3em;
-			  white-space:nowrap;
-			  height:100px;
 		  }
 		  #container{
 			  margin-left: auto;
@@ -29,7 +20,7 @@
 		  .bar-icon {
 			  margin-left:10px;
 		  }
-		  #user-account{
+		  .word{
 			  overflow:hidden;
 		  }
 		  #accordion2 {
@@ -52,68 +43,66 @@
 	  <script src="lib/js/jquery.js"></script>
 	  <script src="lib/js/bootstrap-transition.js"></script>
 	  <script src="lib/js/bootstrap-collapse.js"></script>
-     	  <script src="lib/js/bootstrap-modal.js"></script>
+	  <script src="lib/js/bootstrap-modal.js"></script>
 	
 </head>
 
 <body>
-	<div id="container">
-		<header>
-			<h1 class="demo-panel-title">BlueRise</h1>
-			<div class="accordion" id="accordion2">
-				<div class="accordion-group" >
-				   	<div class="accordion-heading row">
-				   		<div class="todo-icon fui-man-16" style="padding:0;margin:5px 5px 0 40px;"></div>
-				   		<div id="user-account" class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
-				   			example@example.com 
-				   		</div>
-				   	</div>
-				   	<div id="collapseOne" class="accordion-body collapse">
-				   		<div class="todo-icon fui-settings-16" style="padding:0px;margin:5px 5px 0 20px;"></div>
-				   		<div class="accordion-inner">
-				   			User setting
-				   		</div>
-				   		<div class="todo-icon fui-video-16" style="padding:0px;margin:5px 5px 0 20px;"></div>
-				   		<div class="accordion-inner">
-				   			Device
-				   		</div>
-				   		<div class="todo-icon fui-cross-16" style="padding:0px;margin:5px 5px 0 20px;"></div>
-				   		<div class="accordion-inner">
-				   			Log-out
-				   		</div>
-				   	</div>
-				</div>
-			</div>
-			<div class="span12" style="width:960px;">
-				<div class="navbar navbar-inverse">
-					<div class="navbar-inner">
-						<div class="container">
-							<div class="nav-collapse collapse">
-								<ul class="nav">
-									<li>
-										<a href="/">
-											Home
-										</a>
-									</li>
-									<li >
-										<a href="/support.php">
-											Support
-										</a>
-									</li>
-									<li>
-										<a href="/how.php">
-											How it Works
-										</a>
-									</li>
-									<li>
-										<a href="/about.php">
-											About Us
-										</a>
-									</li>
-								</ul>
-							</div><!--/.nav-collapse -->
-						</div><!--container-->
-					</div><!--navbar-inner-->
-				</div>
-			</div>		
-		</header>
+<div id="container">
+	<h1 class="demo-panel-title">BlueRise</h1>
+	<div class="accordion" id="accordion2">
+	   	<div class="accordion-group" >
+	   		<div class="accordion-heading row">
+	   			<div class="todo-icon fui-man-16" style="padding:0;margin:5px 5px 0 40px;"></div>
+	   			<div id="user-account" class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
+	   				example@example.com 
+	   			</div>
+	   		</div>
+	   		<div id="collapseOne" class="accordion-body collapse">
+	   			<div class="todo-icon fui-settings-16" style="padding:0px;margin:5px 5px 0 20px;"></div>
+	   			<div class="accordion-inner">
+	   				User setting
+	   			</div>
+	   			<div class="todo-icon fui-video-16" style="padding:0px;margin:5px 5px 0 20px;"></div>
+	   			<div class="accordion-inner">
+	   				Device
+	   			</div>
+	   			<div class="todo-icon fui-cross-16" style="padding:0px;margin:5px 5px 0 20px;"></div>
+	   			<div class="accordion-inner">
+	   				Log-out
+	   			</div>
+	   		</div>
+	   	</div>
+	</div>
+	<div class="span12" style="width:960px;">
+		<div class="navbar navbar-inverse">
+			<div class="navbar-inner">
+				<div class="container">
+					<div class="nav-collapse collapse">
+						<ul class="nav">
+							<li>
+								<a href="/">
+									Home
+								</a>
+							</li>
+							<li >
+								<a href="/support.php">
+									Support
+								</a>
+							</li>
+							<li>
+								<a href="/how.php">
+									How it Works
+								</a>
+							</li>
+							<li>
+								<a href="/about.php">
+									About Us
+								</a>
+							</li>
+						</ul>
+					</div><!--/.nav-collapse -->
+				</div><!--container-->
+			</div><!--navbar-inner-->
+		</div>
+	</div>
