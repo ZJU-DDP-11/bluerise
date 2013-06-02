351ca3f64e64eef0fa239e49c7c9d6329ce066d0
LouisYe <yeluyuptd@163.com>
2013-06-03 00:47:53 +0800

    change _manage_device.php to safer way in execusql

--- _manage_device.php
+++ _manage_device.php
@@ -20,27 +20,43 @@ if ($_GET['delete'] == 1) {
 	$delete_id = $_GET['id'];
 }
 
+$stmt = mysqli_stmt_init($dbcon);  //initial the $stmt
+
 if ($delete_id != null) {
-	$result = mysqli_query($dbcon, "select * from $tb_device where id=$delete_id;");
+	$query = "select * from ? where id= ? ;";
+	mysqli_stmt_prepare($stmt, $query);
+	mysqli_stmt_bind_param($stmt, 'si', $tb_device, $delete_id); //Set the parameters
+	mysqli_stmt_execute($stmt) or die("failed to find the deleting device");  //execution!
+
+	$result = mysqli_stmt_get_result($stmt);
 	$row = mysqli_fetch_array($result);
 	if ($userid == $row['userid']) {
-		mysqli_query($dbcon, "update $tb_device active=0 where id=$delete_id;");
+		$query = "update ? set active=0 ? where id= ? ;";
+		mysqli_stmt_prepare($stmt, $query);
+		mysqli_stmt_bind_param($stmt, 'si', $tb_device, $delete_id);
+		mysqli_stmt_execute($stmt) or die("failed to delete the device");  //execution!
 	}
 }
 else if (!$deviceid) { //Create new deivce
-	$cmd  = "insert into $tb_device(id, deviceName, description, userid, latitude, longitude, altitude, active) ";
-	$cmd .= "values(null, '$deviceName', '$description', $userid, null, null, null, 1);";
-	mysqli_query($dbcon, $cmd) or die("Can not add new device in database!");
+	$query  = "insert into ?(id, deviceName, description, userid, latitude, longitude, altitude, active) ";
+	$query .= "values(null, '?', '?', ?, null, null, null, 1);";
+	mysqli_stmt_prepare($stmt, $query);
+	mysqli_stmt_bind_param($stmt, 'sssi', $tb_device, $deviceName, $description, $userid);
+	mysqli_stmt_execute($stmt) or die("Can not add new device in database!");
 }
 else {
-	$cmd  = "update $tb_device set ";
-	$cmd .= "deviceName = '$deviceName', ";
-	$cmd .= "description = '$description', ";
-	$cmd .= "longitude = '$longitude', ";
-	$cmd .= "latitude = '$latitude' ";
-	$cmd .= "where userid = $userid and id = $deviceid ;";
-	mysqli_query($dbcon, $cmd);
+	$query  = "update ? set ";
+	$query .= "deviceName = '?', ";
+	$query .= "description = '?', ";
+	$query .= "longitude = '?', ";
+	$query .= "latitude = '?' ";
+	$query .= "where userid = ? and id = ? ;";
+	mysqli_stmt_prepare($stmt, $query);
+	mysqli_stmt_bind_param($stmt,'sssddii', $tb_device, $deviceName, $description, $longitude, $latitude, $userid, $deviceid);
+	mysqli_stmt_execute($stmt) or die("Can not add new device in database!"); //execution!
 }
+
+mysqli_stmt_close($stmt);
 
 gotoThePage($devicepage);
 exit;
