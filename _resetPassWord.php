80b4b46910338b5df87fe64c5e2bc4c7c32ff829
wyz <wyz0011@hotmail.com>
2013-06-04 15:05:53 +0800

    modificaion

--- _resetPassWord.php
+++ _resetPassWord.php
@@ -7,7 +7,7 @@ echo $userid;
 //$stmt=mysqli_prepare($dbcon,"UPDATE  user SET password='PASSWORD(?)'  WHERE id=?;DELETE  FROM setpassword WHERE  id=?");
 //mysqli_stmt_bind_param($stmt, 'sii', $passWord, $userid,$userid);
 //mysqli_stmt_execute($stmt);
-$result=mysqli_query($dbcon,"UPDATE  user SET password='PASSWORD($passWord)'  WHERE id='$userid';DELETE  FROM setpassword WHERE  id='$userid'");
+$result=mysqli_query($dbcon,"UPDATE  user SET password=PASSWORD($passWord)  WHERE id='$userid';DELETE  FROM setpassword WHERE  id='$userid'");
 $row=mysqli_affected_rows($dbcon);
 if($row!=0)
 echo "<script>alert('change password success');location.href='index.php';</script>";
