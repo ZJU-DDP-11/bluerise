56475cc942affbdce0b8b5726ccfa294ac60b183
wyz <wyz0011@hotmail.com>
2013-06-04 14:07:15 +0800

    a little change on changepassword

--- _resetPassWord.php
+++ _resetPassWord.php
@@ -2,8 +2,11 @@
 $userid=$_POST['userid'];
 $passWord=$_POST['password'];
 include("_func_info.php");
-$result=mysqli_query($dbcon,"UPDATE  user SET password=PASSWORD($passWord)  WHERE id='$userid';DELETE  FROM setpassword WHERE  id='$userid'");
-if(mysqli_affected_rows($dbcon)!=0)
+$stmt=mysqli_prepare($dbcon,"UPDATE  user SET password=PASSWORD(?)  WHERE id=?;DELETE  FROM setpassword WHERE  id=?");
+mysqli_stmt_bind_param($stmt, 'sii', $passWord, $userid,$userid);
+mysqli_stmt_execute($stmt);
+//$result=mysqli_query($dbcon,"UPDATE  user SET password=PASSWORD($passWord)  WHERE id='$userid';DELETE  FROM setpassword WHERE  id='$userid'");
+if(mysqli_stmt_affected_rows($stmt)!=0)
 echo "<script>alert('change password success');location.href='index.php';</script>";
 
 
