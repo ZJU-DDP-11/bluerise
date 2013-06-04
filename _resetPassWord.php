12b07e0812706bbbf594407765de58b98a868a5b
wyz <wyz0011@hotmail.com>
2013-06-04 15:16:21 +0800

    modification

--- _resetPassWord.php
+++ _resetPassWord.php
@@ -4,12 +4,17 @@ $passWord=$_POST['password'];
 include("_func_info.php");
 echo $passWord;
 echo $userid;
-//$stmt=mysqli_prepare($dbcon,"UPDATE  user SET password='PASSWORD(?)'  WHERE id=?;DELETE  FROM setpassword WHERE  id=?");
-//mysqli_stmt_bind_param($stmt, 'sii', $passWord, $userid,$userid);
-//mysqli_stmt_execute($stmt);
-$result=mysqli_query($dbcon,"UPDATE  user SET password=PASSWORD($passWord)  WHERE id='$userid';DELETE  FROM setpassword WHERE  id='$userid'");
-$row=mysqli_affected_rows($dbcon);
-if($row!=0)
+$stmt=mysqli_prepare($dbcon,"UPDATE  user SET password='PASSWORD(?)'  WHERE id=?;");
+mysqli_stmt_bind_param($stmt, 'si', $passWord, $userid);
+mysqli_stmt_execute($stmt);
+$row_affect=mysqli_stmt_affected_rows($stmt);
+mysqli_stmt_close($stmt);
+$stmt2=mysqli_prepare($dobcon,"DELETE  FROM setpassword WHERE  id=?");
+mysqli_stmt_bind_param($stmt2, 'i', $userid);
+mysqli_stmt_execute($stmt2);
+//$result=mysqli_query($dbcon,"UPDATE  user SET password=PASSWORD($passWord)  WHERE id='$userid';DELETE  FROM setpassword WHERE  id='$userid'");
+//$row=mysqli_affected_rows($dbcon);
+if($row_affect!=0)
 echo "<script>alert('change password success');location.href='index.php';</script>";
 
 
