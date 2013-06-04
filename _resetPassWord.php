e05f40f025e58957b0445c4ba22e49defc7ff46e
wyz <wyz0011@hotmail.com>
2013-06-04 14:33:04 +0800

    test

--- _resetPassWord.php
+++ _resetPassWord.php
@@ -2,6 +2,8 @@
 $userid=$_POST['userid'];
 $passWord=$_POST['password'];
 include("_func_info.php");
+echo $passWord;
+echo $userid;
 $stmt=mysqli_prepare($dbcon,"UPDATE  user SET password=PASSWORD(?)  WHERE id=?;DELETE  FROM setpassword WHERE  id=?");
 mysqli_stmt_bind_param($stmt, 'sii', $passWord, $userid,$userid);
 mysqli_stmt_execute($stmt);
