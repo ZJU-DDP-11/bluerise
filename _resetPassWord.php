0c669d2933cc9c4e0a05d4f4390c17a9abdb0ad4
wyz <wyz0011@hotmail.com>
2013-06-04 13:55:22 +0800

    test2

--- _resetPassWord.php
+++ _resetPassWord.php
@@ -2,11 +2,9 @@
 $userid=$_POST['userid'];
 $passWord=$_POST['password'];
 include("_func_info.php");
-$result=mysqli_query($dbcon,"UPDATE  user SET password='$passWord'  WHERE id='$userid';DELETE  FROM setpassword WHERE  id='$userid'");
-echo $passWord;
-echo $userid;
+$result=mysqli_query($dbcon,"UPDATE  user SET password=PASSWORD($passWord)  WHERE id='$userid';DELETE  FROM setpassword WHERE  id='$userid'");
 if(mysqli_affected_rows($dbcon)!=0)
-//echo "<script>alert('change password success');location.href='index.php';</script>";
+echo "<script>alert('change password success');location.href='index.php';</script>";
 
 
 ?>
