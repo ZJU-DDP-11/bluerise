7c206009f58c27e3c9c0b6a07673b9e931925a12
csvenja <c.svenjax@gmail.com>
2013-06-04 13:44:21 +0800

    removed useless script

--- user_settings.php
+++ user_settings.php
@@ -122,26 +122,5 @@
 			<div class="row demo-row"></div>
 		</div><!--container-->
 	</form>
-
-	<!--<script src="lib/js/jquery-1.8.2.min.js"></script>
-	<script src="lib/js/jquery-ui-1.10.0.custom.min.js"></script>
-	<script src="lib/js/jquery.dropkick-1.0.0.js"></script>
-	<script src="lib/js/custom_checkbox_and_radio.js"></script>
-	<script src="lib/js/custom_radio.js"></script>
-	<script src="lib/js/jquery.tagsinput.js"></script>
-	<script src="lib/js/bootstrap-tooltip.js"></script>
-	<script src="lib/js/jquery.placeholder.js"></script>
-	<script src="http://vjs.zencdn.net/c/video.js"></script>
-	<script src="lib/js/application.js"></script>
-	<script src="lib/js/jquery.js"></script>
-	<script type="text/javascript">
-	  var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-	  document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-	</script>
-	<script type="text/javascript">
-	  try{
-		var pageTracker = _gat._getTracker("UA-19972760-2");
-		pageTracker._trackPageview();
-		} catch(err) {}
-	</script>-->
+	
 <?php include('footer.php'); ?>
