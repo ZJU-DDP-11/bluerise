6dfd8bbac8fe870ab746a4f433453f0a3399935d
wyz <wyz0011@hotmail.com>
2013-06-04 14:59:41 +0800

    modification

--- _resetPassWord.php
+++ _resetPassWord.php
@@ -4,11 +4,11 @@ $passWord=$_POST['password'];
 include("_func_info.php");
 echo $passWord;
 echo $userid;
-$stmt=mysqli_prepare($dbcon,"UPDATE  user SET password='PASSWORD(?)'  WHERE id=?;DELETE  FROM setpassword WHERE  id=?");
-mysqli_stmt_bind_param($stmt, 'sii', $passWord, $userid,$userid);
-mysqli_stmt_execute($stmt);
-//$result=mysqli_query($dbcon,"UPDATE  user SET password='PASSWORD($passWord)'  WHERE id='$userid';DELETE  FROM setpassword WHERE  id='$userid'");
-$row=mysqli_stmt_affected_rows($stmt);
+//$stmt=mysqli_prepare($dbcon,"UPDATE  user SET password='PASSWORD(?)'  WHERE id=?;DELETE  FROM setpassword WHERE  id=?");
+//mysqli_stmt_bind_param($stmt, 'sii', $passWord, $userid,$userid);
+//mysqli_stmt_execute($stmt);
+$result=mysqli_query($dbcon,"UPDATE  user SET password='PASSWORD($passWord)'  WHERE id='$userid';DELETE  FROM setpassword WHERE  id='$userid'");
+$row=mysqli_affected_rows($dbcon);
 if($row!=0)
 echo "<script>alert('change password success');location.href='index.php';</script>";
 
