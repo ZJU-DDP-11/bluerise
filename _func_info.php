d2e60d0785a582d1dd60229c2fd5177522a58aeb
Xhacker Liu <liu.dongyuan@gmail.com>
2013-06-04 13:50:49 +0800

    Using remote database.

--- _func_info.php
+++ _func_info.php
@@ -1,23 +1,23 @@
 <?php
-$homepage	= "index.php";
-$devicepage	= "device.php";
+$homepage       = "index.php";
+$devicepage     = "device.php";
 $editdevicepage = "edit_device.php";
-$device_detail = "device_detail.php";
-$_mdevicepage	= "_manage_device.php";
+$device_detail  = "device_detail.php";
+$_mdevicepage   = "_manage_device.php";
 
-$dbhost 	= "localhost";
-$dbaccount	= "ddp";
-$dbpassword	= "ddprocks";
-$dbname		= "ddp";
-$tb_device	= "device";
-$tb_user	= "user";
-$tb_data	= "data";
-$tb_datatype	= "datatype";
+$dbhost         = "10.71.10.71";
+$dbaccount      = "ddp";
+$dbpassword     = "ddprocks";
+$dbname         = "ddp";
+$tb_device      = "device";
+$tb_use         = "user";
+$tb_data        = "data";
+$tb_datatype    = "datatype";
 
-$dbcon=mysqli_connect($dbhost, $dbaccount, $dbpassword, $dbname) or die("Fail to connect to database: ".mysqli_connect_error());
+$dbcon = mysqli_connect($dbhost, $dbaccount, $dbpassword, $dbname) or die("Fail to connect to database: " . mysqli_connect_error());
 
 function gotoThePage($page) { //Not appropriate for the pure PHP page without HTML outputs;
-		echo "<script language='javascript' type='text/javascript'> window.location.href='".$page."'</script>";
-	}
+	echo "<script language='javascript' type='text/javascript'> window.location.href='" . $page . "'</script>";
+}
 
 ?>
